282f0507577b1834f45c6621b6a308abbb63511f
Fikri Arrayan <arrayan407@gmail.com>
2026-04-28 14:49:35 +0700

    nambah Midlleware dan route tugas 6

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\GenreController;
 use Illuminate\Support\Facades\Validator;
 
 Route::apiResource('authors', AuthorController::class);
-
+Route::apiResource('authors', AuthorController::class)->only(['index','show']);
 
 
 Route::get('/books', [BookController::class, 'index']);
@@ -15,4 +15,9 @@ Route::post('/books', [BookController::class, 'store']);
 
 
 Route::apiResource('genres', GenreController::class);
+Route::apiResource('genres', GenreController::class)->only(['index','show']);
 
+Route::middleware('admin')->group(function () {
+    Route::apiResource('authors', AuthorController::class)->except(['index','show']);
+    Route::apiResource('genres', GenreController::class)->except(['index','show']);
+});
